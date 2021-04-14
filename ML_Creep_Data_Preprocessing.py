2c1ac7b813724feb14c9b354d48b894609d92ce8
Muhammad Stracey <strmuh001@gmail.com>
2021-04-14 15:26:39 +0200

    Add method for saving csv

--- ML_Creep_Data_Preprocessing.py
+++ ML_Creep_Data_Preprocessing.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as py
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 import warnings
 
 warnings.filterwarnings('ignore', category=FutureWarning)
@@ -19,17 +16,26 @@ class DataPreProcessing:
         # Split Data into features and labels
         features = self.expdata.drop('Strain', axis=1)
         labels = self.expdata['Strain'] * 1000
+
         # Form train, test and validate datasets
         X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.4, random_state=42)
         X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
-        # Write data to csv files
-        X_train.to_csv('train_features_' + self.name + '.csv', index=False)
-        X_val.to_csv('val_features_' + self.name + '.csv', index=False)
-        X_test.to_csv('test_features_' + self.name + '.csv', index=False)
+        processeddata = {'train_features_': X_train, 'val_features_': X_val, 'test_features_': X_test,
+                         'train_labels_': y_train, 'val_labels_': y_val, 'test_labels_': y_test}
+        return processeddata
 
-        y_train.to_csv('train_labels_' + self.name + '.csv', index=False)
-        y_val.to_csv('val_labels_' + self.name + '.csv', index=False)
-        y_test.to_csv('test_labels_' + self.name + '.csv', index=False)
+    def savedata(self):
+        # Write data to csv files
+        pdata = self.splitdata()
+        for k, v in pdata.items():
+            v.to_csv(k + self.name + '.csv', index=False)
+        # X_train.to_csv('train_features_' + self.name + '.csv', index=False)
+        # X_val.to_csv('val_features_' + self.name + '.csv', index=False)
+        # X_test.to_csv('test_features_' + self.name + '.csv', index=False)
+        #
+        # y_train.to_csv('train_labels_' + self.name + '.csv', index=False)
+        # y_val.to_csv('val_labels_' + self.name + '.csv', index=False)
+        # y_test.to_csv('test_labels_' + self.name + '.csv', index=False)
 
 
 if __name__ == '__main__':
